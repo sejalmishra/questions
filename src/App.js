4391cb941b0cd56363d3e25fff2d0f36f2df4e78
sejalmishra <sejalmishra1310@gmail.com>
2021-05-11 15:53:25 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [questions, setQuestions] = useState(data);
+  return <main>
+    <article>
+    <div className='container'>
+      <h2>Questions And Answers About Login</h2>
+      <section className='info'>
+      {questions.map((question) => {
+        return <SingleQuestion key={question.id} {...question} />
+      })}
+      </section>
+      </div>
+    </article>
+  </main>
+  ;
 }
 
 export default App;
+
+
